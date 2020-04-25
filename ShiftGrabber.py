ea6d78ca4c2fafa246b2f8cf36b405c82d789bf2
dhcGU <dhcGU@github.com>
2020-04-25 12:41:35 -0400

    can listen for groupme messages

--- ShiftGrabber.py
+++ ShiftGrabber.py
@@ -9,12 +9,11 @@ load_dotenv()
 GROUPME_API_KEY = os.environ.get("GROUPME_API_KEY", "Need enviornment variable GROUPME_API_KEY")
 GROUP_ID = os.environ.get("GROUP_ID", "Need environment variable GROUP_ID")
 
-client = Client.from_token("3ec4dfe0672501385dda422a9716c2d0")
+user_id = 28092669
 #"Test" group is is 59490587
 groups = list(client.groups.list_all())
 for group in groups:
     print(group.name)
-API_ENDPOINT = "https://api.groupme.com/v3/bots?token=3ec4dfe0672501385dda422a9716c2d0"
 
 def message_group(message, group_id, API_KEY):
     url = f"https://api.groupme.com/v3/groups/{group_id}/messages?token={API_KEY}"
@@ -23,9 +22,51 @@ def message_group(message, group_id, API_KEY):
     return response
 
 
+url = "https://push.groupme.com/faye"
+body1 = {
+        "id":"1",
+        "channel":"/meta/handshake",
+        "successful":True,
+        "version":"1.0",
+        "supportedConnectionTypes":["long-polling","cross-origin-long-polling","callback-polling","websocket","in-process"],
+        "clientId":"0wgyczg0sbd91m0uc8wyv09qblos",
+        "advice": {"reconnect":"retry","interval":0,"timeout":30000}
+    }
 
 
-response = message_group("testing", GROUP_ID, GROUPME_API_KEY)
+body2 = {
+    "channel":"/meta/subscribe",
+    "clientId":"37413m11wuk0e0nngcny1p1mf570iyl9wi0",
+    "subscription":"/user/28092669",
+    "id":"2",
+    "ext":
+      {
+        "access_token":f"{GROUPME_API_KEY}",
+        "timestamp":1322556419
+      }
+  }
+      
+      
+      
+body3 = {
+    "channel":"/meta/connect",
+    "clientId":"37413m11wuk0e0nngcny1p1mf570iyl9wi0",
+    "connectionType":"long-polling",
+    "id":"4"
+  }
+id = 5      
+while True:
+    body3 = {
+    "channel":"/meta/connect",
+    "clientId":"37413m11wuk0e0nngcny1p1mf570iyl9wi0",
+    "connectionType":"long-polling",
+    "id":f"{id}"
+    }
+    response = requests.post(url, json=body3)
+    print(response.status_code)
+    id += 1
 
 
-print(response.status_code)
+
+
+
