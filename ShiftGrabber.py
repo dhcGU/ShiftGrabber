102063d643120761db34ca1e590b2d6f1aa9706f
dhcGU <dhcGU@github.com>
2020-04-26 12:45:06 -0400

    can automatically respond to specific request format

--- ShiftGrabber.py
+++ ShiftGrabber.py
@@ -1,9 +1,10 @@
-from groupy import Client
-from groupy.api import bots
+
 import requests
 import json
 import os
 from dotenv import load_dotenv
+import threading
+import re
 
 load_dotenv()
 GROUPME_API_KEY = os.environ.get("GROUPME_API_KEY", "Need enviornment variable GROUPME_API_KEY")
@@ -11,9 +12,6 @@ GROUP_ID = os.environ.get("GROUP_ID", "Need environment variable GROUP_ID")
 
 user_id = 28092669
 #"Test" group is is 59490587
-groups = list(client.groups.list_all())
-for group in groups:
-    print(group.name)
 
 def message_group(message, group_id, API_KEY):
     url = f"https://api.groupme.com/v3/groups/{group_id}/messages?token={API_KEY}"
@@ -21,10 +19,37 @@ def message_group(message, group_id, API_KEY):
     response = requests.post(url, json=jsonMessage)
     return response
 
+def parse_response_thread(response, schedule):
+    if(get_groupme_groupid(response) == GROUP_ID):
+        text = get_groupme_text(response).lower()
+        if ("can anyone" in text):
+            time = re.search("\d+:\d+.*\d+:\d+", text)
+            for key in schedule.keys():
+                if(key in text):
+                    if(time.group() in schedule[key]):
+                        message_group("Yea I'll take it", GROUP_ID, GROUPME_API_KEY)
+            
+def get_groupme_text(respone):
+    return json.loads(response.text)[1]["data"]["subject"]["text"]
+
+def get_groupme_groupid(response):
+    return json.loads(response.text)[1]["data"]["subject"]["group_id"]
+
+
+user_schedule = {
+        "monday" : ["16:00-20:00"],
+        "tuesday" : [], 
+        "wednesday" : [],
+        "thursday" : [],
+        "friday" : [],
+        "saturday" : [],
+        "sunday" : [],
+        }
 
 url = "https://push.groupme.com/faye"
+id = 1
 body1 = {
-        "id":"1",
+        "id":f"{id}",
         "channel":"/meta/handshake",
         "successful":True,
         "version":"1.0",
@@ -33,37 +58,34 @@ body1 = {
         "advice": {"reconnect":"retry","interval":0,"timeout":30000}
     }
 
-
+response = requests.post(url, json=body1)
+print(response.status_code)
+client_id = json.loads(response.text)[0]["clientId"]
+id += 1
 body2 = {
     "channel":"/meta/subscribe",
-    "clientId":"37413m11wuk0e0nngcny1p1mf570iyl9wi0",
-    "subscription":"/user/28092669",
-    "id":"2",
+    "clientId":f"{client_id}",
+    "subscription":f"/user/{user_id}",
+    "id":f"{id}",
     "ext":
       {
         "access_token":f"{GROUPME_API_KEY}",
         "timestamp":1322556419
       }
   }
-      
-      
-      
-body3 = {
-    "channel":"/meta/connect",
-    "clientId":"37413m11wuk0e0nngcny1p1mf570iyl9wi0",
-    "connectionType":"long-polling",
-    "id":"4"
-  }
-id = 5      
+
+response = requests.post(url, json=body2)
 while True:
     body3 = {
     "channel":"/meta/connect",
-    "clientId":"37413m11wuk0e0nngcny1p1mf570iyl9wi0",
+    "clientId":f"{client_id}",
     "connectionType":"long-polling",
     "id":f"{id}"
     }
     response = requests.post(url, json=body3)
     print(response.status_code)
+    parser = threading.Thread(target=parse_response_thread, args=(response,user_schedule))
+    parser.start()
     id += 1
 
 
