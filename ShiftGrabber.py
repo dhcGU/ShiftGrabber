68f23c9652bde938efe1dd7f749d0fbfc0a32f5b
dhcGU <dhcGU@github.com>
2020-05-04 18:35:01 -0400

    Can continuously listen for and respond to requests to cover shifts

--- ShiftGrabber.py
+++ ShiftGrabber.py
@@ -22,11 +22,11 @@ def message_group(message, group_id, API_KEY):
 def parse_response_thread(response, schedule):
     if(get_groupme_groupid(response) == GROUP_ID):
         text = get_groupme_text(response).lower()
-        if ("can anyone" in text):
+        if ("can anyone" in text or ("looking" in text and "cover" in text)):
             time = re.search("\d+:\d+.*\d+:\d+", text)
             for key in schedule.keys():
                 if(key in text):
-                    if(time.group() in schedule[key]):
+                    if("".join(time.group().split()) in schedule[key]):
                         message_group("Yea I'll take it", GROUP_ID, GROUPME_API_KEY)
             
 def get_groupme_text(respone):
@@ -35,59 +35,72 @@ def get_groupme_text(respone):
 def get_groupme_groupid(response):
     return json.loads(response.text)[1]["data"]["subject"]["group_id"]
 
-
-user_schedule = {
-        "monday" : ["16:00-20:00"],
-        "tuesday" : [], 
-        "wednesday" : [],
-        "thursday" : [],
-        "friday" : [],
-        "saturday" : [],
-        "sunday" : [],
+def setup_connection(id, url):
+    id = 1
+    body1 = {
+            "id":f"{id}",
+            "channel":"/meta/handshake",
+            "successful":True,
+            "version":"1.0",
+            "supportedConnectionTypes":["long-polling","cross-origin-long-polling","callback-polling","websocket","in-process"],
+            "clientId":"0wgyczg0sbd91m0uc8wyv09qblos",
+            "advice": {"reconnect":"retry","interval":0,"timeout":30000}
         }
+    
+    response = requests.post(url, json=body1)
+    print(response.status_code)
+    client_id = json.loads(response.text)[0]["clientId"]
+    id += 1
+    body2 = {
+        "channel":"/meta/subscribe",
+        "clientId":f"{client_id}",
+        "subscription":f"/user/{user_id}",
+        "id":f"{id}",
+        "ext":
+          {
+            "access_token":f"{GROUPME_API_KEY}",
+            "timestamp":1322556419
+          }
+      }
+    response = requests.post(url, json=body2)
+    return client_id
+          
+def get_day(day):
+    shifts = input("Please enter your availability for " + day + ": ")
+    return shifts.split(", ")
+
+def get_schedule():
+    schedule = {}
+    days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+    for day in days:
+        schedule[day.lower()] = get_day(day)
+    return schedule
+
 
 url = "https://push.groupme.com/faye"
-id = 1
-body1 = {
-        "id":f"{id}",
-        "channel":"/meta/handshake",
-        "successful":True,
-        "version":"1.0",
-        "supportedConnectionTypes":["long-polling","cross-origin-long-polling","callback-polling","websocket","in-process"],
-        "clientId":"0wgyczg0sbd91m0uc8wyv09qblos",
-        "advice": {"reconnect":"retry","interval":0,"timeout":30000}
-    }
-
-response = requests.post(url, json=body1)
-print(response.status_code)
-client_id = json.loads(response.text)[0]["clientId"]
-id += 1
-body2 = {
-    "channel":"/meta/subscribe",
-    "clientId":f"{client_id}",
-    "subscription":f"/user/{user_id}",
-    "id":f"{id}",
-    "ext":
-      {
-        "access_token":f"{GROUPME_API_KEY}",
-        "timestamp":1322556419
-      }
-  }
-
-response = requests.post(url, json=body2)
+print("Please enter your availability as a comma separated list  of 4 hour blocks in Military Time")
+print("ie. if available from 8:00AM-12:00PM and 4:00PM-Midnight, enter '8:00-12:00, 16:00-20:00, 20:00-2400'")
+user_schedule = get_schedule()
+id = 0
+client_id =setup_connection(id, url)
 while True:
-    body3 = {
-    "channel":"/meta/connect",
-    "clientId":f"{client_id}",
-    "connectionType":"long-polling",
-    "id":f"{id}"
-    }
-    response = requests.post(url, json=body3)
-    print(response.status_code)
-    parser = threading.Thread(target=parse_response_thread, args=(response,user_schedule))
-    parser.start()
-    id += 1
-
+    try:
+        body3 = {
+        "channel":"/meta/connect",
+        "clientId":f"{client_id}",
+        "connectionType":"long-polling",
+        "id":f"{id}"
+        }
+        response = requests.post(url, json=body3)
+        print(response.status_code)
+        parser = threading.Thread(target=parse_response_thread, args=(response, user_schedule))
+        parser.start()
+        id += 1
+    except KeyboardInterrupt:
+        break
+    except:
+        print("Reconnecting")
+        client_id = setup_connection(id, url)        
 
 
 
